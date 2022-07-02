4068d2e4feb5ffdba2c92279945572a47612759f
Ntuthuko <ntuthukorocks@gmail.com>
2022-07-02 20:58:39 +0200

    Leave type management module - data tables - font awesome - auto mapping -js prompt for delete

--- Data/LeaveType.cs
+++ Data/LeaveType.cs
@@ -1,8 +1,11 @@
-﻿namespace LeaveManagement.Web.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Data
 {
     public class LeaveType: BaseEntity
     {
         public string Name { get; set; }
+        [Display(Name = "Default number of days")]
         public int DefaultDays { get; set; }
     }
 }
